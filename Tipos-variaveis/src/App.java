49e2e1244639088a1e5925329c8d943265b62317
Robert Junior <robertdiasjunior9495@gmail.com>
2024-05-13 22:23:22 -0300

    iniciando operadores

--- Tipos-variaveis/src/App.java
+++ Tipos-variaveis/src/App.java
@@ -1,5 +1,22 @@
+import javax.print.DocFlavor.STRING;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+       STRING meuNome = "Robert Dias";
+        
+        int numero = 10;
+
+        numero = 20;
+
+        numero = 30;
+
+        numero = 50;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI =3,14;
+
+
     }
 }
---
+++ Tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+  public static void main(String[] args) {
+    int a, b;
+
+    a = 60;
+    b = 60;
+
+    String resultado = a == b ? "verdadeiro" : "falso";
+
+    System.out.println(resultado);
+
+  }
+}
